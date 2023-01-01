a9f3720e276b2b703ed66294bda2195a649d1778
Saul Johnson <saul.a.johnson@gmail.com>
2023-01-01 21:38:57 +0100

    Document GitHub ribbon component

--- src/components/gitHubRibbon.tsx
+++ src/components/gitHubRibbon.tsx
@@ -1,14 +1,27 @@
+/**
+ * Contains the {@link GitHubRibbon} component.
+ *
+ * @since 01/01/2022
+ * @author Saul Johnson <saul.a.johnson@gmail.com>
+ */
+
+import Image from "next/image";
+
+
+/**
+ * Represents a GitHub ribbon displayed in the bottom-right corner of the page.
+ */
 const GitHubRibbon: React.FC = () => {
     return (
         <a href="https://github.com/lambdacasserole/classr" target="_blank" rel="noreferrer">
-            <img src="/images/github-ribbon.svg" style={{
+            <Image src="/images/github-ribbon.svg" alt="Open-source on GitHub" style={{
                 position: 'fixed',
                 bottom: 0,
                 right: 0,
                 zIndex: 10,
                 width: '94px',
                 height: 'auto',
-            }} />
+            }} width={94} height={94} />
         </a>
     );
 }
